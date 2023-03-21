362c1ef5dce5c0dd4581e6d8b97de7dd68dbe770
huuxxx <hdowling87@hotmail.com>
2023-03-21 18:16:55 +1000

    Create Helpers.cs

---
+++ Helpers.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+public static class Helpers
+{
+    public static Quaternion TransformLocalRotationToQuaternion(Transform transform)
+    {
+        Quaternion result = Quaternion.Euler(transform.localRotation.eulerAngles.x, transform.localRotation.eulerAngles.y, transform.localRotation.eulerAngles.z);
+        return result;
+    }
+
+    public static bool IsLocationPathable(Transform targetLocation, float? searchRadius = 30f)
+    {
+        if (NavMesh.SamplePosition(targetLocation.position, out NavMeshHit target, (float)searchRadius, NavMesh.AllAreas))
+        {
+            return target.hit;
+        }
+
+        return false;
+    }
+
+    public static bool IsObjectLayerInLayerMask(GameObject obj, LayerMask layerMask)
+    {
+        int objectLayerMaskToInt = (1 << obj.layer);
+
+        if ((layerMask.value & objectLayerMaskToInt) > 0) return true;
+        else return false;
+    }
+}
